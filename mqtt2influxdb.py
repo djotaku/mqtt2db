9aa93db2db712c66149eeae960473882107bd904
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-14 13:01:58 -0400

    per understanding of how InfluxDB should be used, I've changed the database name to be static and the measurement name to reflect the date. The only other valid idea based on what I read would be to use tags, but I think this works well enough

--- mqtt2influxdb.py
+++ mqtt2influxdb.py
@@ -6,15 +6,15 @@ from datetime import datetime
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%dT%H:%M:%S")
 date_string = now.strftime("%Y-%m-%d")
-databasename = f'smoker-{date_string}'
-print(f"databasename:{databasename}")
+measurementname = f'smoker-{date_string}'
+print(f"measurementname:{measurementname}")
 
 topics = ['outTopic','smoker/temp','smoker/WiFi/#']
 
 #database setup
 client = InfluxDBClient(host='tanukimario', port=8086)
 for databases in client.get_list_database():
-    if databases['name'] == databasename:
+    if databases['name'] == "smoker":
         print("there's a match!")
     else:
         client.create_database(databasename)
@@ -30,7 +30,7 @@ while True:
             print(f'{message.topic}:{tempF}° F')
             now = datetime.utcnow()
             dt_string = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-            data = [{"measurement":"smoker data", "time":f"{dt_string}", "fields":{"temperature":f"{tempF}", "Fan Speed":"not a real value yet"}}]
+            data = [{"measurement":f"{measurementname}", "time":f"{dt_string}", "fields":{"temperature":f"{tempF}", "Fan Speed":"not a real value yet"}}]
             client.write_points(data)
         else:
             print(f'{message.topic}:{message.payload}')
